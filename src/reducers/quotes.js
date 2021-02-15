75ebc93f950a70d674e9d9af73d39b0477cf8ce2
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-14 18:22:17 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,12 @@
-export default (state = [], action) => {
-  return state;
+export default (state = [
+
+], action) => {
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+   
+    default:
+      return state
+  }
+
 }
